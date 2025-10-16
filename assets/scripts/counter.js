a143627fbefeb1b06fa8203df784668142e106ca
ruollin <ruollin@users.noreply.github.com>
2025-10-16 03:01:32 +0000

    deploy: 222b683907f6baaf25b9b8cad8ad8103fdde9f6c

---
+++ assets/scripts/counter.js
@@ -0,0 +1,6 @@
+var sc_project=13176333; 
+var sc_invisible=0; 
+var sc_security="26a9a308"; 
+var scJsHost = "https://";
+document.write("<sc"+"ript type='text/javascript' src='" + scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
